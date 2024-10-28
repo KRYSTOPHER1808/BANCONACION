996ef1f9622fabb35f691cf07abef150532b0227
Mateo William <mateobarbuso@gmail.com>
2024-10-28 12:01:20 -0500

    MI INYTERFAZ ROBERT

--- BANCONACION/Vista/ACBGmail.aspx.cs
+++ BANCONACION/Vista/ACBGmail.aspx.cs
@@ -1,22 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace BANCONACION.Vista
 {
-    public partial class ACBGmail : System.Web.UI.Page
+    public partial class ACBGmail : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            // Lógica para abrir cuenta bancaria
         }
+
+        protected void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            // Lógica si se cambia el RadioButton4
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            // Muestra el modal al hacer clic en "términos y condiciones"
+            Response.Redirect("TYC.aspx?");
+        }
+
+       
     }
 }
