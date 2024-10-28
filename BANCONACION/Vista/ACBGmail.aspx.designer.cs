b7fe5f87e677eee55487ee1f9b93c932bed5439a
Mateo William <mateobarbuso@gmail.com>
2024-10-28 10:13:51 -0500

    hhh

--- BANCONACION/Vista/ACBGmail.aspx.designer.cs
+++ BANCONACION/Vista/ACBGmail.aspx.designer.cs
@@ -24,12 +24,12 @@ namespace BANCONACION.Vista
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control Button1.
+        /// Control Label1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
